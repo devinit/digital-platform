6f0015c7259bbfa04c77b30534da6ce492733f02
dw8547 <dw8547@users.noreply.github.com>
2017-01-31 08:42:19 +0000

    Updated the data source for in-oda-net.csv.
    The data is now presented in 2015 constant prices.

--- nodejs/js/datamap_raw.js
+++ nodejs/js/datamap_raw.js
@@ -28,7 +28,8 @@ exports.csv=
  "country-year/in-ha.csv": "data_series.\"in_ha\"",
  // changed the source of data, now presented in 2015 constant prices & 2015 to be included
  "country-year/in-oda-gross.csv": "  ( SELECT * FROM fact.\"in_oda_gross_2015\" WHERE year<=2015 ) sq ",
- "country-year/in-oda-net.csv": "fact.\"in_oda_net_2012\"",
+ // changed the source of data, now presented in 2015 constant prices
+ "country-year/in-oda-net.csv": "fact.\"in_oda_net_2015\"",
  "country-year/in-oof-gross.csv": "data_series.\"in_oof_gross\"",
  "country-year/intl-flows-donors.csv": "data_series.\"intl_flows_donors\"",
  "country-year/intl-flows-recipients.csv": "data_series.\"intl_flows_recipients\"",
